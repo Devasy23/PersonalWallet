4c70b6b524c80577e0561655aa3c40e851a80942
Devasy Patel <110348311+Devasy23@users.noreply.github.com>
2024-04-11 22:43:46 +0530

    Revert "Updated the file in response to the PR comment"
    This reverts commit 05856a28eb5d01a8249c107209759ab1eb853a8b.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,24 +1,15 @@
-# app.py
-from fastapi import FastAPI
-
-def include_routers(app: FastAPI, routers: list) -> None:
-    for router in routers:
-        app.include_router(router['router'], prefix=router['prefix'], tags=router['tags'])
-
-# main.py
 import uvicorn
-from app import include_routers
-from api.v1.endpoints import user, transactions, debts, accounts
+from api.v1.endpoints import accounts, debts, transactions, user
+from fastapi import FastAPI
 
 app = FastAPI()
 
-routers = [
-    {'router': user.router, 'prefix': '/api/v1/user', 'tags': ['User']},
-    {'router': transactions.router, 'prefix': '/api/v1/transactions', 'tags': ['Transactions']},
-    {'router': debts.router, 'prefix': '/api/v1/debts', 'tags': ['Debts']},
-    {'router': accounts.router, 'prefix': '/api/v1/accounts', 'tags': ['Accounts']}
-]
-
-include_routers(app, routers)
+app.include_router(user.router, prefix="/api/v1/user", tags=["User"])
+app.include_router(
+    transactions.router, prefix="/api/v1/transactions", tags=["Transactions"]
+)
+app.include_router(debts.router, prefix="/api/v1/debts", tags=["Debts"])
+app.include_router(accounts.router, prefix="/api/v1/accounts", tags=["Accounts"])
 
 if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
