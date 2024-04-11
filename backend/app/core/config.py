290d829457da2096836fd0f8b7e88aa3205d4f0b
Devasy Patel <110348311+Devasy23@users.noreply.github.com>
2024-04-11 22:18:33 +0530

    Refactor database configuration in config.py to use environment variables

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,7 +1,14 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "mongodb://localhost:27017"
-DATABASE_NAME = "your_database_name"
+try:
+    load_dotenv()
+except Exception as e:
+    print(f"Error loading .env file: {e}")
+    
+DATABASE_URL = f"mongodb+srv://{os.getenv('MONGO_USERNAME')}:{os.getenv('MONGO_PASSWORD')}@devasy23.a8hxla5.mongodb.net/?retryWrites=true&w=majority&appName=Devasy23"
+DATABASE_NAME = "Pwallet"
 client = AsyncIOMotorClient(DATABASE_URL)
 db = client[DATABASE_NAME]
 
